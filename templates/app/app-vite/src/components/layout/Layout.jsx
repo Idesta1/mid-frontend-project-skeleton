19d91da89b0fd10614baec687718ea3407c1acac
Iglesia Desta <iglesiad25@gmail.com>
2026-09-10 20:43:24 +0200

    clean footer code

--- templates/app/app-vite/src/components/layout/Layout.jsx
+++ templates/app/app-vite/src/components/layout/Layout.jsx
@@ -1,5 +1,6 @@
 import styles from "./Layout.module.css";
 import Navbar from "./Navbar.jsx";
+import Footer from "./Footer.jsx";
 import { Outlet } from "react-router-dom";
 
 export default function Layout({ children }) {
@@ -9,48 +10,7 @@ export default function Layout({ children }) {
 
       <main>{children ? children : <Outlet />}</main>
 
-      <footer className={styles["app-footer"]}>
-        <div className={styles["footer-grid"]}>
-          <section className={styles["footer-column"]}>
-            <h3>About Us</h3>
-            <ul>
-              <li>Our Mission</li>
-              <li>Team</li>
-              <li>Careers</li>
-            </ul>
-          </section>
-
-          <section className={styles["footer-column"]}>
-            <h3>Connect With Us</h3>
-            <ul>
-              <li>Instagram</li>
-              <li>LinkedIn</li>
-              <li>Contact support</li>
-            </ul>
-          </section>
-
-          <section className={styles["footer-column"]}>
-            <h3>Plan Events</h3>
-            <ul>
-              <li>Create event</li>
-              <li>Pricing</li>
-              <li>Venue tips</li>
-            </ul>
-          </section>
-
-          <section className={styles["footer-column"]}>
-            <h3>Manage Event</h3>
-            <ul>
-              <li>Dashboard</li>
-              <li>Attendee list</li>
-              <li>Ticket check-in</li>
-            </ul>
-          </section>
-        </div>
-        <p className={styles["footer-copy"]}>
-          Copyright © 2026 HackYourFuture Denmark
-        </p>
-      </footer>
+      <Footer />
     </div>
   );
 }
